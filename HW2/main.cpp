fd8108128951dc96997aa8eaeab07dd9986c79aa
caffrey928 <caffreyacnologialawace@gmail.com>
2024-03-22 16:29:04 +0800

    [HW2] revise plot in server & delete useless code in main.cpp

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,32 +1,8 @@
-/* Sockets Example
- * Copyright (c) 2016-2020 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "mbed.h"
 #include "wifi_helper.h"
 #include "mbed-trace/mbed_trace.h"
 #include "stm32l475e_iot01_accelero.h"
 #define SCALE_MULTIPLIER 1
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-#include "root_ca_cert.h"
-
-#ifndef DEVICE_TRNG
-#error "mbed-os-example-tls-socket requires a device which supports TRNG"
-#endif
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
 
 class SocketDemo {
     static constexpr size_t MAX_NUMBER_OF_ACCESS_POINTS = 10;
@@ -57,18 +33,6 @@ public:
             return;
         }
 
-        /* if we're using a wifi interface run a quick scan */
-        if (_net->wifiInterface()) {
-            /* the scan is not required to connect and only serves to show visible access points */
-            wifi_scan();
-
-            /* in this example we use credentials configured at compile time which are used by
-             * NetworkInterface::connect() but it's possible to do this at runtime by using the
-             * WiFiInterface::connect() which takes these parameters as arguments */
-        }
-
-        /* connect will perform the action appropriate to the interface type to connect to the network */
-
         printf("Connecting to the network...\r\n");
 
         nsapi_size_or_error_t result = _net->connect();
@@ -85,15 +49,6 @@ public:
             printf("Error! _socket.open() returned: %d\r\n", result);
             return;
         }
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-        result = _socket.set_root_ca_cert(root_ca_cert);
-        if (result != NSAPI_ERROR_OK) {
-            printf("Error: _socket.set_root_ca_cert() returned %d\n", result);
-            return;
-        }
-        _socket.set_hostname(MBED_CONF_APP_HOSTNAME);
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
 
         /* now we have to find where to connect */
 
@@ -126,7 +81,7 @@ public:
 
         BSP_ACCELERO_Init();
 
-        while(count<10) {
+        while(count<30) {
             ++count;
             printf("\nNew loop, LED1 should blink during sensor read\n");
 
@@ -146,7 +101,7 @@ public:
                 printf("Error sending: %d\n",response);
             }
 
-            ThisThread::sleep_for(1000);
+            ThisThread::sleep_for(500ms);
 
         }
         /* ---------------------------------------*/
@@ -170,88 +125,6 @@ private:
         printf("%s address is %s\r\n", hostname, (address.get_ip_address() ? address.get_ip_address() : "None") );
 
         return true;
-    }
-
-    bool send_http_request()
-    {
-        /* loop until whole request sent */
-        const char buffer[] = "GET / HTTP/1.1\r\n"
-                              "Host: ifconfig.io\r\n"
-                              "Connection: close\r\n"
-                              "\r\n";
-
-        nsapi_size_t bytes_to_send = strlen(buffer);
-        nsapi_size_or_error_t bytes_sent = 0;
-
-        printf("\r\nSending message: \r\n%s", buffer);
-
-        while (bytes_to_send) {
-            bytes_sent = _socket.send(buffer + bytes_sent, bytes_to_send);
-            if (bytes_sent < 0) {
-                printf("Error! _socket.send() returned: %d\r\n", bytes_sent);
-                return false;
-            } else {
-                printf("sent %d bytes\r\n", bytes_sent);
-            }
-
-            bytes_to_send -= bytes_sent;
-        }
-
-        printf("Complete message sent\r\n");
-
-        return true;
-    }
-
-    bool receive_http_response()
-    {
-        char buffer[MAX_MESSAGE_RECEIVED_LENGTH];
-        int remaining_bytes = MAX_MESSAGE_RECEIVED_LENGTH;
-        int received_bytes = 0;
-
-        /* loop until there is nothing received or we've ran out of buffer space */
-        nsapi_size_or_error_t result = remaining_bytes;
-        while (result > 0 && remaining_bytes > 0) {
-            result = _socket.recv(buffer + received_bytes, remaining_bytes);
-            if (result < 0) {
-                printf("Error! _socket.recv() returned: %d\r\n", result);
-                return false;
-            }
-
-            received_bytes += result;
-            remaining_bytes -= result;
-        }
-
-        /* the message is likely larger but we only want the HTTP response code */
-
-        printf("received %d bytes:\r\n%.*s\r\n\r\n", received_bytes, strstr(buffer, "\n") - buffer, buffer);
-
-        return true;
-    }
-
-    void wifi_scan()
-    {
-        WiFiInterface *wifi = _net->wifiInterface();
-
-        WiFiAccessPoint ap[MAX_NUMBER_OF_ACCESS_POINTS];
-
-        /* scan call returns number of access points found */
-        int result = wifi->scan(ap, MAX_NUMBER_OF_ACCESS_POINTS);
-
-        if (result <= 0) {
-            printf("WiFiInterface::scan() failed with return value: %d\r\n", result);
-            return;
-        }
-
-        printf("%d networks available:\r\n", result);
-
-        for (int i = 0; i < result; i++) {
-            printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\r\n",
-                   ap[i].get_ssid(), get_security_string(ap[i].get_security()),
-                   ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
-                   ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5],
-                   ap[i].get_rssi(), ap[i].get_channel());
-        }
-        printf("\r\n");
     }
 
     void print_network_info()
--- HW2/server.py
+++ HW2/server.py
@@ -6,22 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 HOST = '192.168.0.106' # IP address
 PORT = 5000 # Port to listen on (use ports > 1023)
 
-# Prepare the 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-plt.ion()  # Turn on interactive mode
 plt.show()
-
-# Define limits for the axes
-ax.set_xlim([-1000, 1000])
-ax.set_ylim([-1000, 1000])
-ax.set_zlim([-1000, 1000])
-
-# Axis labels
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -41,14 +26,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     choose += 1
                 data = buffer_data[choose] + '}'
             obj = json.loads(data)
+            
             t = obj['s']
-            #plot.scatter(t, obj['x'], c='blue') # x, y, z, gx, gy, gz
-            #plot.xlabel("sample num")
-            #plot.pause(0.0001)
-            # Extract accelerometer data
-            ACCELERO_X = float(obj['x'])
-            ACCELERO_Y = float(obj['y'])
-            ACCELERO_Z = float(obj['z'])
-            ax.scatter(ACCELERO_X, ACCELERO_Y, ACCELERO_Z, c='blue')
-            plt.draw()
+            plt.scatter(t, obj['x'], c='blue') # x, y, z, gx, gy, gz
+            plt.scatter(t, obj['y'], c='red') # x, y, z, gx, gy, gz
+            plt.scatter(t, obj['z'], c='green') # x, y, z, gx, gy, gz
+            plt.xlabel("time")
+            plt.ylabel("acceleration")
             plt.pause(0.0001)
